7c8636a6ccbd5683f646f806d23cb9b7cb6a9f9c
Hstarorg <hm910705@163.com>
2017-04-23 23:23:08 +0800

    Improve api project build

--- backend-api/gulpfile.js
+++ backend-api/gulpfile.js
@@ -3,44 +3,56 @@ const gulp = require('gulp4');
 const devServer = require('gulp-develop-server');
 const notifier = require('node-notifier');
 
+let needInstallDep = process.argv.indexOf('-i') >= 0;
+
 const notify = msg => {
-	notifier.notify({
-		title: 'Web-Dojo Server',
-		message: msg
-	});
+  notifier.notify({
+    title: 'Web-Dojo Server',
+    message: msg
+  });
 };
 
 gulp.task('clean', done => {
-	rm('-rf', 'dist');
-	done();
+  rm('-rf', 'dist');
+  done();
 });
 
 gulp.task('copy', () => {
-	return gulp.src('src/**/*')
-		.pipe(gulp.dest('dist'));
+  return gulp.src([
+    'src/**/*',
+    'package.json'
+  ])
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('serve', done => {
-	devServer.listen({ path: './dist/index.js' }, err => {
-		if (err) { console.error(err); }
-		done();
-	});
+  devServer.listen({ path: './dist/index.js' }, err => {
+    if (err) { console.error(err); }
+    done();
+  });
 });
 
 gulp.task('restart', done => {
-	devServer.restart(err => {
-		if (err) { console.error(err); }
-		done();
-	});
+  devServer.restart(err => {
+    if (err) { console.error(err); }
+    done();
+  });
 });
 
 gulp.task('watch', done => {
-	let watcher = gulp.watch([
-		'./src/**/*',
-		'!./src/database/**/*'
-	]);
-	watcher.on('all', gulp.series('copy', 'restart', () => notify('Restart server succeed.')));
-	done();
+  let watcher = gulp.watch([
+    './src/**/*',
+    '!./src/database/**/*'
+  ]);
+  watcher.on('all', gulp.series('copy', 'restart', () => notify('Restart server succeed.')));
+  done();
 });
 
 gulp.task('default', gulp.series('clean', 'copy', 'serve', 'watch'));
+
+gulp.task('installDep', done => {
+  exec('cd dist && npm i --production');
+  done();
+});
+
+gulp.task('release', gulp.series('clean', 'copy', ...(needInstallDep ? ['installDep'] : [])));
